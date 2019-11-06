7d7911be682ebd791b3fe1e4f6065267e81dc6cf
Christoffer Lundberg <lundbergarn2@hotmail.com>
2019-11-06 08:19:27 +0100

    Updates

---
+++ precache-manifest.84fefa2afd3333015a4e29455e926d52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "409c00db1763b5b4baae5a3096bef305",
+    "url": "/Todo_Drag_And_Drop/index.html"
+  },
+  {
+    "revision": "2d7371f3930b89ec498b",
+    "url": "/Todo_Drag_And_Drop/static/css/main.85f4e3eb.chunk.css"
+  },
+  {
+    "revision": "20d36b5d2bcc8e563de9",
+    "url": "/Todo_Drag_And_Drop/static/js/2.06ccf17d.chunk.js"
+  },
+  {
+    "revision": "2d7371f3930b89ec498b",
+    "url": "/Todo_Drag_And_Drop/static/js/main.4c6e3c30.chunk.js"
+  },
+  {
+    "revision": "15e30aeede24a3847cc0",
+    "url": "/Todo_Drag_And_Drop/static/js/runtime-main.a3218895.js"
+  }
+]);
